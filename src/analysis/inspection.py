fb79b5e4fa9f5cef03f9240c3cdfce411e2872ca
mobadara <muyiwa.j.obadara@gmail.com>
2025-10-14 15:25:45 +0100

    Data Analysis updated

--- src/analysis/inspection.py
+++ src/analysis/inspection.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import seaborn as sns
 from IPython.display import display
+
 
 class Inspector:
     """
@@ -21,13 +23,37 @@ class Inspector:
         display(self.data.info())
         return self
         
-    def summary_statistics(self, all: bool = True) -> 'Inspector':
+    def statistics(self, all: bool = True) -> 'Inspector':
         """
         Provides summary statistics of the dataset.
         """
         print('\nSummary Statistics:' + (' (including categorical data)' if all else ' (numerical data only)'))
         print('-' * 30)
         display(self.data.describe(include='all') if all else self.data.describe())
+        
+        print('\nUnique Values per Categorical Column:')
+        print('-' * 30)
+        discrete_cols = self.data.select_dtypes(include=['object', 'category', 'int']).columns
+        for col in discrete_cols:
+            unique_counts = self.data[col].nunique()
+            # Display only if unique values are less than 10
+            if unique_counts < 10: 
+                print(f'\nColumn: {col} (Unique Values: {unique_counts})')
+                print('-' * 30)
+                display(self.data[col].unique())
+            else:
+                print("There are more than 10 unique values.")
+        return self
+    
+    def duplicates(self) -> 'Inspector':
+        """
+        Checks for duplicate rows in the dataset.
+        """
+        duplicates = self.data.duplicated().sum()
+        if duplicates > 0:
+            print(f'\nNumber of duplicate rows: {duplicates}')
+        else:
+            print('\nNo duplicate rows found.')
         return self
     
     def missing_values(self) -> 'Inspector':
@@ -40,6 +66,9 @@ class Inspector:
             print('\nMissing Values:')
             print('-' * 30)
             display(missing)
+            print('\nMissing Values Heatmap:')
+            print('-' * 30)
+            sns.heatmap(self.data.isnull(), cbar=False, yticklabels=False, cmap='viridis')
         else:
             print('\nNo missing values found.')
         return self
