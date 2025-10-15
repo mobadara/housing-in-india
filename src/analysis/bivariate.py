c5e8d4588f8aca560dce0afaa475b48461e62d2d
mobadara <muyiwa.j.obadara@gmail.com>
2025-10-15 14:21:07 +0100

    bivariate analysis

---
+++ src/analysis/bivariate.py
@@ -0,0 +1,63 @@
+"""
+This is a module for bivariate analysis of a the housing dataset.
+It contains methods to analyze the relationship between two variables -- the target
+and each feature in the dataset.
+"""
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+plt.style.use('ggplot')
+
+
+class Bivariate:
+    def __init__(self, df: pd.DataFrame, target: str = 'TARGET(PRICE_IN_LACS)'):
+        self.df = df
+        self.target = target
+        
+        
+    def numerical_analysis(self, column: str):
+        """
+        Performs bivariate analysis between the target
+        and a numerical feature.
+        
+        Arg:
+            column (str): The name of the numerical column to analyze.
+        """
+        numeric_cols = self.df.select_dtypes(include=['number']).columns
+        if column not in self.df.columns:
+            raise ValueError(f"Column '{column}' not found in DataFrame.")
+        
+        if column not in numeric_cols:
+            raise ValueError(f"Column '{column}' is not numerical.")
+        
+        plt.figure(figsize=(10,6))
+        ax = sns.scatterplot(data=self.df, x=column, y=self.target, color='skyblue')
+        ax.set_xlabel(f'{column}')
+        ax.set_ylabel('f{self.target}')
+        ax.set_title(f'Scatter Plot of SalePrice vs {column}')
+        plt.show()
+        
+        
+    def categorial_analysis(self, column: str):
+        """
+        Performs bivariate analysis between the target
+        and a categorical feature.
+        
+        Arg:
+            column (str): The name of the categorical column to analyze.
+        """
+        categorical_cols = self.df.select_dtypes(include=['object', 'category']).columns
+        if column not in self.df.columns:
+            raise ValueError(f"Column '{column}' not found in DataFrame.")
+        
+        if column not in categorical_cols:
+            raise ValueError(f"Column '{column}' is not categorical.")
+        
+        plt.figure(figsize=(10,6))
+        ax = sns.boxplot(data=self.df, hue=column, y=self.target, palette='pastel')
+        ax.set_xlabel(f'{column}')
+        ax.set_ylabel(f'{self.target}')
+        ax.set_title(f'Box Plot of {self.target} vs {column}')
+        plt.xticks(rotation=45)
+        plt.show()
--- src/analysis/univariate.py
+++ src/analysis/univariate.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from typing import Tuple
+from matplotlib.container import BarContainer
+from matplotlib.axes import Axes
 plt.style.use('ggplot')
 
 class Univariate:
@@ -8,7 +11,7 @@ class Univariate:
         self.df = df
         
         
-    def categorial_analysis(self, column: str):
+    def categorial_analysis(self, column: str) -> Tuple[BarContainer, 'Univariate']:
         """
         Perform univariate analysis on a categorical column.
         
@@ -26,14 +29,13 @@ class Univariate:
         
         total = sum(self.df[column].value_counts().values)
         plt.figure(figsize=(10, 6))
-        
-        bars = plt.bar(self.df[column].value_counts().index,
-                        self.df[column].value_counts().values,
+        x = self.df[column].value_counts().index
+        y = self.df[column].value_counts().values   
+        bars = plt.bar(x, y,
                         color=sns.color_palette('pastel')[0:5],
                         alpha=0.7,
                         edgecolor='black',
-                        width=0.6,
-                        label=f'Count of {column}',
+                        width=0.3
                         )
         
         for bar in bars:
@@ -49,4 +51,25 @@ class Univariate:
         plt.xticks(rotation=45)
         plt.tight_layout()
         plt.show()
-        return self
+        return bars, self
+    
+    
+    def numerical_analysis(self, column: str) -> Tuple[Axes, 'Univariate']:
+        """
+        Performs bivariate analysis on a numerical column.
+        
+        Arg:
+            column (str): The name of the numerical column to analyze.
+        """
+        if column not in self.df.columns:
+            raise ValueError(f"Column '{column}' not found in DataFrame.")
+        
+        if column not in self.df.select_dtypes(include=['number']).columns:
+            raise ValueError(f"Column '{column}' is not numerical.")
+        
+        plt.figure(figsize=(10,6))
+        ax = sns.histplot(data=self.df, x=column, kde=True, bins=10, color='skyblue')
+        ax.set_xlabel(f'{column}')
+        ax.set_title(f'Distribution of {column}')
+        plt.show()
+        return ax, self
